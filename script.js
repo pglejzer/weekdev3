ea5b890cf3248563e2fe875b74a38a5121d9bc7d
Piotr Glejzer <glejzerpiotr@gmail.com>
2018-05-11 11:20:42 +0200

    changed navigation

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-
-
 document.addEventListener("DOMContentLoaded", function(){
-    var menu = document.getElementById('menu'),
+    var menu = document.getElementById("menu"),
         menuButton = document.getElementById('menu-button');
     menuButton.addEventListener('click', function(){
 
-        if(menu.className == 'nav__ul'){
-            menu.classList.add('nav__ul_responsive');
+        if(menu.className == "nav__ul"){
+            menu.classList.add('nav__ul--responsive');
             this.firstChild.alt = "hide menu";
         }
         else{
-            menu.classList.add(newFunction());
+            menu.classList.add('nav__ul--hide');
             setTimeout(function(){
-                menu.classList.remove('nav__ul_responsive');
-                menu.classList.remove('nav__ul_hide');
+                menu.classList.remove('nav__ul--responsive');
+                menu.classList.remove('nav__ul--hide');
             }, 600);
             this.firstChild.alt = "show menu";
         }
@@ -21,19 +19,26 @@ document.addEventListener("DOMContentLoaded", function(){
 
 });
 
+
 document.addEventListener("DOMContentLoaded", function(){
     var nav = document.getElementById('nav');
     window.addEventListener('scroll', function(){
         var currentPosition = pageYOffset;
         if(currentPosition > 100){
-            nav.classList.add('nav_sticky');
+            nav.classList.add('nav__sticky');
         }
         else{
-            nav.classList.remove('nav_sticky');
+            nav.classList.remove('nav__sticky');
         }
     });
 });
 
-function newFunction() {
-    return 'nav__ul_hide';
-}
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 900);
+});
+
